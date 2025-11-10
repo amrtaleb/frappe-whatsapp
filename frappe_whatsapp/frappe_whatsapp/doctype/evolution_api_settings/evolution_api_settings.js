fcee1c5190c12b374a4577d24d6cf526569c4ffd
AmrTaleb98 <dev.amr@axis-inspection.com>
2025-11-10 22:10:56 +0300

    Initial commit

---
+++ frappe_whatsapp/frappe_whatsapp/doctype/evolution_api_settings/evolution_api_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Shridhar Patil and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Evolution API Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
